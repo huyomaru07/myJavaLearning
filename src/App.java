7e5fd6b1a9ba75e429f05c8224782c1d1bb8e54a
huyomaru07 <nchauthanhhuy@gmail.com>
2026-02-21 22:22:45 -0700

    Initial commit

--- src/App.java
+++ src/App.java
@@ -1,4 +1,14 @@
 public class App {
+    public static int countDown(int n) {
+        if (n == 0) {
+            System.out.println("Blast off!");
+        } else {
+            System.out.println(n);
+            countDown(n - 1);
+        }
+        return n;
+    }
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
