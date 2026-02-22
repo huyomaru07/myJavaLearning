5ed8bebbde5046cd5cba480f36cdc94cad99b1dc
huyomaru07 <nchauthanhhuy@gmail.com>
2026-02-21 22:45:34 -0700

    Update changes on removeEven branch

--- src/App.java
+++ src/App.java
@@ -9,6 +9,17 @@ public class App {
         }
         return n;
     }
+    public static int removeEven(int[] arr, int index) {
+        if (index >= arr.length) {
+            return 0;
+        }
+        int count = removeEven(arr, index + 1);
+        if (arr[index] % 2 == 0) {
+            return count + 1;
+        } else {
+            return count;
+        }
+    }
     
 
     public static void main(String[] args) throws Exception {
